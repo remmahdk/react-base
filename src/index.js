ca2210e2ac8ac854d34800aaca5a38cc3de6fdef
Peter Hammer Mortensen <67020755+remmahdk@users.noreply.github.com>
2021-01-20 14:24:53 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App';
+import App from './App';
+import './styles.scss';
+import './App.css';
 
 ReactDOM.render(<App />, document.getElementById('app'));
